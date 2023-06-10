a56524ef77fcd041f7999b11c18dc1757e53e2bd
Rifat <rifathossain328663@gmail.com>
2023-06-10 19:16:57 +0600

    Add project files.

---
+++ Models/PokemonOwner.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonRevewApp.Models
+{
+    public class PokemonOwner
+    {
+        public int PokemonId { get; set; }
+        public int OwnerId { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Owner Owner { get; set; }
+
+    }
+}
