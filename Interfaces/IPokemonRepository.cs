0e47200e6169d9e7d80aa02c55063b24f5da2cc6
Rifat <rifathossain328663@gmail.com>
2023-06-15 22:52:23 +0600

    impliment IPokemon interfac and end points

--- Interfaces/IPokemonRepository.cs
+++ Interfaces/IPokemonRepository.cs
@@ -5,6 +5,11 @@ namespace PokemonRevewApp.Interfaces
     public interface IPokemonRepository
     {
         ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        decimal GetPokemonRating(int pokeId);
+        bool PokemonExists(int pokeId);
+
 
     }
 }
--- Repository/PokemonRepository.cs
+++ Repository/PokemonRepository.cs
@@ -4,6 +4,7 @@ using PokemonRevewApp.Models;
 
 namespace PokemonRevewApp.Repository
 {
+
     public class PokemonRepository : IPokemonRepository
     {
         private readonly DataContext context;
@@ -12,10 +13,31 @@ namespace PokemonRevewApp.Repository
         {
             this.context = context;
         }
+
+        public Pokemon GetPokemon(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            throw new NotImplementedException();
+        }
+
         //
         public ICollection<Pokemon> GetPokemons()
         {
             return context.Pokemon.OrderBy(p=>p.Id).ToList();
+        }
+
+        public bool PokemonExists(int pokeId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
