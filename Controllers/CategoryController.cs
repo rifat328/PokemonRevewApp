ac617b9f9f9b790798360147913e04158059b6d5
Rifat <rifathossain328663@gmail.com>
2023-06-20 02:16:44 +0600

    Category controller and Dto

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using PokemonRevewApp.Dto;
+using PokemonRevewApp.Interfaces;
+using PokemonRevewApp.Models;
+using PokemonRevewApp.Repository;
+
+namespace PokemonRevewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository=categoryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type=typeof(IEnumerable<Category>))]
+        public IActionResult GetCategories()
+        {
+            var categories = _mapper.Map<List<CategoryDto>>(_categoryRepository.GetCategories());
+
+            if (!ModelState.IsValid)
+
+                return BadRequest(ModelState);
+            return Ok(categories);
+
+            
+        }
+
+        [HttpGet("{categoryId}")]
+        [ProducesResponseType(200, Type = typeof(Category))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int categoryId)
+        {
+            //Validation
+            if (!_categoryRepository.CategoryExists(categoryId))
+            {
+                return NotFound();
+            }
+            var category = _mapper.Map<CategoryDto>(_categoryRepository.GetCategory(categoryId));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(category);
+
+        }
+        //   https://youtu.be/bSvYErXVRtQ?t=1001
+    }
+}
