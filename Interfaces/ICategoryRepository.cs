b66af9cb5f29eb08613762e0ae342533bc60038a
Rifat <rifathossain328663@gmail.com>
2023-07-07 11:20:54 +0600

    Reviewed ...

--- Interfaces/ICategoryRepository.cs
+++ Interfaces/ICategoryRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace PokemonRevewApp.Interfaces
+﻿using PokemonRevewApp.Models;
+
+namespace PokemonRevewApp.Interfaces
 {
     public interface ICategoryRepository
     {
+        ICollection<Category>GetCategories();
+        Category GetCategory(int id);
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+        bool CategoryExists(int id);
+
     }
 }
--- Repository/CategoryRepository.cs
+++ Repository/CategoryRepository.cs
@@ -1,6 +1,35 @@
-﻿namespace PokemonRevewApp.Repository
+﻿using PokemonRevewApp.Data;
+using PokemonRevewApp.Interfaces;
+using PokemonRevewApp.Models;
+
+namespace PokemonRevewApp.Repository
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        private DataContext _context;
+        public CategoryRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(c => c.Id == id);
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            return _context.Categories.ToList();
+        }
+
+        public Category GetCategory(int id)
+        {
+            return _context.Categories.Where(e => e.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Pokemon> GetPokemonByCategory(int categoryId)
+        {
+            return _context.PokemonCategories.Where(e => e.CategoryId == categoryId).Select(c => c.Pokemon).ToList();//select of include for navigational property.
+
+        }
     }
 }
